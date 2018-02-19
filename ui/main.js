649e54bd782ed374d67203f951dc65a0ef6b273f
Anand Simmy <anandsimmy7@gmail.com>
2018-02-19 21:30:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ var img= document.getElementById('img');
 var marginLeft=0;
 function moveLeft() {
     marginLeft=marginLeft+10;
-    img.style.marginLeft= marginleft + 'px'
+    img.style.marginLeft= marginLeft + 'px'
 }
 img.onclick = function() {
     var interval= setInterval(moveLeft,100);
