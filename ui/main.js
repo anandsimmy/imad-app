697697e03402c540a9038bd7684e51037736659e
Anand Simmy <anandsimmy7@gmail.com>
2018-02-20 20:04:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,7 +17,7 @@ button.onclick = function () {
     
         }
     }
-  request.open('GET','http://anandsimmy7.imad.hasura-app.io/', true);
+  request.open('GET','http://anandsimmy7.imad.hasura-app.io/counter', true);
   request.send(null);
    
 };
