1476ec9b60e1ca8c66dd9f5064676c68eade4506
Anand Simmy <anandsimmy7@gmail.com>
2018-03-02 12:37:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,9 +141,9 @@ app.get('/submit-name', function(req,res){
 app.get('/articles/:articleName',function(req,res){
     
     
-    //where title = ' " + req.params.articleName + "'"
+    //
     
-    pool.query("SELECT * from article ", function(err, result){ 
+    pool.query("SELECT * FROM article WHERE title = ' " + req.params.articleName + "'", function(err, result){ 
        if(err){
           res.status(500).send(err.toString());
       }else{
